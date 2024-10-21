378d0ec29573fb8c40ea8da0df76c1a003fc858d
Matheo <mflorez3@live.com>
2024-10-21 00:03:47 -0400

    inital set-up for edit form component and sending new title name back to parent file

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import {useState} from "react"
 
-const BookEdit = () => {
-  return <div>BookEdit</div>;
+const BookEdit = ({ book }) => {
+  const [editTitleVal, setEditTitleVal] = useState(book.title);
+
+  const handleChange = (event) => {
+    setEditTitleVal(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('adding new title:', editTitleVal);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>New Title</label>
+      <input
+        className="rounded shadow"
+        onChange={handleChange}
+        type="text"
+        value={editTitleVal}
+      />
+      <button className="border-blue-500 border-solid border-2 rounded px-1 hover:bg-blue-500 hover:text-white transition transform duration-100 ease-in-out active:bg-blue-600">
+        Save
+      </button>
+    </form>
+  );
 };
 
 export default BookEdit;
--- src/components/BookShow.jsx
+++ src/components/BookShow.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
+import BookEdit from './BookEdit';
 
 const BookShow = ({ book, deleteBook }) => {
-  const handleClick = () => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleShowEdit = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleDeleteClick = () => {
     deleteBook(book.id);
   };
 
   return (
     <div className="flex flex-col justify-between bg-white w-48 h-48 rounded shadow p-2 mb-2">
-      <h3 className="underline font-medium tracking-wider">Book Title</h3>
-      {book.title}
+      <div className="flex justify-between">
+        <h3 className="underline font-medium tracking-wider">Book Title</h3>
+        <button
+          onClick={handleShowEdit}
+          className="border-green-500 border-solid border-2 rounded px-1 hover:bg-green-500 hover:text-white transition transform duration-100 ease-in-out active:bg-green-600"
+        >
+          Edit
+        </button>
+      </div>
+      {showEdit ? <BookEdit book={book} /> : <p>{book.title}</p>}
+
       <button
-        onClick={handleClick}
+        onClick={handleDeleteClick}
         className="border-red-300 border-solid border-2 rounded hover:bg-red-300 hover:text-white transition transform duration-100 ease-in-out active:bg-red-400"
       >
         Delete
